433768dd8554cd1eaebdf8136f60b4cbe2003fa7
MaxMontanus <32044@ma-web.nl>
2021-11-03 11:42:12 +0100

    Push

---
+++ PYTB1L2InVariablesWeTrust/InVariablesWeTrust.py
@@ -0,0 +1,16 @@
+charachter = "a"
+
+Float1 = 8.3
+
+Boolean1 = True
+Boolean2 = Boolean1
+
+Integer1 = 3
+Integer2 = 2
+Integer3 = Integer1 - Integer2
+
+String1 ="Yestos "
+String2 ="Barbatross"
+String3 = String1 + String2
+
+print(charachter, Float1, Boolean2, Integer3, String3)
---
+++ PYTB1L3MyLogicalOperations/MyLogicalOperations.py
@@ -0,0 +1,8 @@
+geld = 2000
+werk = True
+leeftijd = 17
+
+if leeftijd > 19 and werk == True or geld > 5000: 
+    print("Verkrijgbaar")
+else :
+    print("Niet verkrijgbaar ")
